1f838314fdb31d9c45bd6fca9edb2c9d415386c9
deepakmench18 <deepakmench18@gmail.com>
2021-07-06 21:42:19 +0530

    Max Integer number

---
+++ genric.java
@@ -0,0 +1,30 @@
+public class MaximumTest<T extends Comparable<T>> {
+	T x,y,z;
+	public MaximumTest(T x,T y,T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public T maximum()
+	{
+		return MaximumTest.maximum(x,y,z);
+		
+	}
+	public static <T extends Comparable<T>> T maximum(T x,T y,T z)
+	{
+		T max = x;
+		if(y.compareTo(max) > 0)
+		{
+			max = y;
+		}
+		if(z.compareTo(max) > 0) {
+			max = z;
+		}
+		printMax(x,y,z,max);
+		return max;
+	}
+public static void main(String args[]) {
+		Integer xInt = 3,yInt = 4,zInt = 5;
+                new MaximumTest<Integer>(xInt,yInt,zInt).maximum();
+}
+}
